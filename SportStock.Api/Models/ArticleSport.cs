6f3c9943bdb81170ddd747510707da4f5c117bfe
Wesley Putman <barryvert@gmail.com>
2026-03-19 10:37:55 +0100

    Test Classe abstraite

---
+++ SportStock.Api/Models/ArticleSport.cs
@@ -0,0 +1,26 @@
+public abstract class ArticleSport{
+    public int Id { get; protected set; }
+    public string Nom { get; protected set; }
+    public decimal Prix { get; protected set; }
+    public int QuantiteEnStock { get; protected set; }
+
+    //ManytoOne Une catégorie peut avoir plusieurs articles
+    public int CategorieId { get; protected set; }
+    public Categorie? Categorie { get; protected set; }
+
+    //OneToOne Un article a un emplacement de stock
+    public EmplacementStock? EmplacementStock { get; set; }
+
+    //ManytoMany Un article peut être fourni par plusieurs fournisseurs et un fournisseur peut fournir plusieurs articles
+    public ICollection<Fournisseur> Fournisseurs { get; protected set; } = new List<Fournisseur>();
+
+    protected ArticleSport(string nom, decimal prix){
+        Nom = nom;
+        Prix = prix;
+        QuantiteEnStock = 0;
+    }
+
+    public void AjouterStock(int quantite){
+        if(quantite > 0) QuantiteEnStock += quantite;
+    }
+}
