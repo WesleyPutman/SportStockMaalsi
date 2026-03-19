02d6b0d678c75a34fdb9a791c0474d567e276ce0
Wesley Putman <barryvert@gmail.com>
2026-03-19 11:17:02 +0100

    Migration et ajout des dépendances dans Program.cs

---
+++ SportStock.Api/Migrations/20260319101602_InitialCreate.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace SportStock.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nom = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Fournisseurs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    NomSociete = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fournisseurs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Articles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nom = table.Column<string>(type: "text", nullable: false),
+                    Prix = table.Column<decimal>(type: "numeric", nullable: false),
+                    QuantiteEnStock = table.Column<int>(type: "integer", nullable: false),
+                    CategorieId = table.Column<int>(type: "integer", nullable: false),
+                    Discriminator = table.Column<string>(type: "character varying(13)", maxLength: 13, nullable: false),
+                    Taille = table.Column<string>(type: "text", nullable: true),
+                    Matiere = table.Column<string>(type: "text", nullable: true),
+                    Couleur = table.Column<string>(type: "text", nullable: true),
+                    Genre = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Articles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Articles_Categories_CategorieId",
+                        column: x => x.CategorieId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ArticleFournisseur",
+                columns: table => new
+                {
+                    ArticlesFournisId = table.Column<int>(type: "integer", nullable: false),
+                    FournisseursId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ArticleFournisseur", x => new { x.ArticlesFournisId, x.FournisseursId });
+                    table.ForeignKey(
+                        name: "FK_ArticleFournisseur_Articles_ArticlesFournisId",
+                        column: x => x.ArticlesFournisId,
+                        principalTable: "Articles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ArticleFournisseur_Fournisseurs_FournisseursId",
+                        column: x => x.FournisseursId,
+                        principalTable: "Fournisseurs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Emplacements",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Zone = table.Column<string>(type: "text", nullable: false),
+                    Allee = table.Column<string>(type: "text", nullable: false),
+                    Rayon = table.Column<string>(type: "text", nullable: false),
+                    Niveau = table.Column<string>(type: "text", nullable: false),
+                    ArticleSportId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Emplacements", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Emplacements_Articles_ArticleSportId",
+                        column: x => x.ArticleSportId,
+                        principalTable: "Articles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ArticleFournisseur_FournisseursId",
+                table: "ArticleFournisseur",
+                column: "FournisseursId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Articles_CategorieId",
+                table: "Articles",
+                column: "CategorieId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Emplacements_ArticleSportId",
+                table: "Emplacements",
+                column: "ArticleSportId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ArticleFournisseur");
+
+            migrationBuilder.DropTable(
+                name: "Emplacements");
+
+            migrationBuilder.DropTable(
+                name: "Fournisseurs");
+
+            migrationBuilder.DropTable(
+                name: "Articles");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
