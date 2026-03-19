96c77104559801c03d5a3dfd1bab8a3889a884d3
Wesley Putman <barryvert@gmail.com>
2026-03-19 10:42:11 +0100

    Ajout classe enfant

---
+++ SportStock.Api/Models/VetementSport.cs
@@ -0,0 +1,13 @@
+public class VetementSport : ArticleSport{
+    public string Taille { get; private set; }
+    public string Matiere { get; private set; }
+    public string Couleur { get; private set; }
+    public string Genre { get; private set; }
+
+    public VetementSport(string nom, decimal prix, string taille, string matiere, string couleur, string genre) : base(nom, prix){
+        Taille = taille;
+        Matiere = matiere;
+        Couleur = couleur;
+        Genre = genre;
+    }
+}
