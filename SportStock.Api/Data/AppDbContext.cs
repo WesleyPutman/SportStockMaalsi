23cd2da003d0cb87d19ebb11b9b8f33bff0cf359
Wesley Putman <barryvert@gmail.com>
2026-03-19 10:58:53 +0100

    DbContext C# -> SQL

---
+++ SportStock.Api/Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using SportStock.Api.Models;
+
+namespace SportStock.Api.Data{
+    public class AppDbContext : DbContext{
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<ArticleSport> Articles { get; set; }
+        public DbSet<Categorie> Categories { get; set; }
+        public DbSet<Fournisseur> Fournisseurs { get; set; }
+        public DbSet<EmplacementStock> Emplacements { get; set; }
+        public DbSet<VetementSport> Vetements { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder){
+            modelBuilder.Entity<ArticleSport>()
+                .HasOne(a => a.Fournisseurs)
+                .WithOne(e => e.Article)
+                .HasForeignKey<EmplacementStock>(e => e.ArticleSportId);
+            modelBuilder.Entity<ArticleSport>()
+                .HasMany(a => a.Fournisseurs)
+                .WithMany(f => f.ArticlesFournis)
+                .UsingEntity(j => j.ToTable("ArticleFournisseur"));
+        }
+    }
+}
