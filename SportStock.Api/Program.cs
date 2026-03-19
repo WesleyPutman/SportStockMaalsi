d5d4db6ac23d39a171eeb36b93c6c2e24940f0b9
Wesley Putman <barryvert@gmail.com>
2026-03-19 11:08:59 +0100

    Ajout config DbContext

--- SportStock.Api/Program.cs
+++ SportStock.Api/Program.cs
@@ -4,6 +4,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// Ajout de la configuration de la base de données PostgreSQL
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
